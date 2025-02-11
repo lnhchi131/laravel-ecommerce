ed32656589f270659748283a1fbf681d5d569a21
Chi <hoangthilinhchi131@gmail.com>
2025-02-12 02:37:31 +0700

    Admin and User Authentication

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,13 @@
 <?php
+
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\AuthAdmin;
+
 
 
 
@@ -9,3 +15,12 @@ use Illuminate\Support\Facades\Auth;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware(['auth' ,AuthAdmin::class])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
